c71e3dabb3c40ef6da7acd870c2378cb828b995a
Víctor González <victor.gonzalez@geomati.co>
2017-10-23 09:35:19 +0200

    Implement webpack loader

--- src/main.js
+++ src/main.js
@@ -1,3 +1,56 @@
-const value = 42;
+const EXPORT_NAME = 'gl';
 
-export default value;
+let getArguments = require('es-arguments');
+
+export function load(app, config, modules) {
+  let allPromises = [];
+
+  config.forEach(env => {
+    let promises = {};
+    let resolves = {};
+
+    let isEnabled = name => !env[name] || env[name].enabled !== false;
+    let enabledModules = Object.keys(modules).filter(isEnabled);
+
+    // Create promises for all active modules
+    enabledModules.forEach(name => {
+      let p = new Promise(resolve => { resolves[name] = resolve; });
+      promises[name] = p;
+      allPromises.push(p);
+    });
+
+    // Load modules and resolve them
+    enabledModules.forEach(name => {
+      let callback = modules[name];
+      let opts = env[name];
+      let deps;
+      if (app.deps && app.deps[name]) {
+        deps = app.deps[name];
+      } else {
+        [, ...deps] = getArguments(callback);
+      }
+
+      let modulePromises = deps
+        .map(d => promises[d])
+        .filter(p => p instanceof Promise);
+
+      Promise.all(modulePromises).then(values => {
+        resolves[name](callback(opts, ...values));
+      });
+    });
+  });
+
+  return Promise.all(allPromises);
+}
+
+export default function (source) {
+  let modules = JSON.parse(source).modules;
+  let names = Object.keys(modules);
+  let imports = names.map(n => `import { ${EXPORT_NAME} as ${n} } from '${modules[n]}';`).join('\n');
+
+  return `
+  ${imports}
+  ${getArguments.toString()}
+  ${load.toString()}
+  export default config => load(${source}, config, { ${names.join(',')} });`;
+}
--- test/main-test.js
+++ test/main-test.js
@@ -1,10 +1,54 @@
 debugger; // eslint-disable-line no-debugger
 
-import module from '../src/main.js';
+import loader from '../src/main.js';
+import { load } from '../src/main.js';
 import assert from 'assert';
 
-describe('module',  function () {
-  it('is 42', function () {
-    assert.equal(42, module);
+const SOURCE = {
+  'modules': {
+    'map': './map',
+    'layers': 'layers-dependency'
+  },
+  'deps': {
+    'layers': ['map']
+  }
+};
+
+let map = (opts) => opts;
+let layers = (opts, mymap) => { mymap.ten = 10; };
+let app = { deps: { layers: ['map'] } };
+
+describe('module', function () {
+  it('imports modules', function () {
+    let ret = loader(JSON.stringify(SOURCE));
+    assert(ret.match(/import { gl as map } from '.\/map'/));
+    assert(ret.match(/import { gl as layers } from 'layers-dependency'/));
+  });
+
+  it('adds load function', function () {
+    let ret = loader(JSON.stringify(SOURCE));
+    assert(ret.match(/function\s*load\s*\(.*\)/));
+  });
+
+  it('exports function', function () {
+    let ret = loader(JSON.stringify(SOURCE));
+    assert(ret.match(/export default config => load\(.*, config, { map,layers }\);/));
+  });
+
+  it('loads modules', function (done) {
+    let config = {
+      map: { boo: 2 }
+    };
+    let test = () => assert.equal(10, config.map.ten);
+    load(app, [config], { map, layers }).then(test).then(done, done);
+  });
+
+  it('disables modules', function (done) {
+    let config = {
+      map: { boo: 2 },
+      layers: { enabled: false }
+    };
+    let test = () => assert(!config.map.ten);
+    load(app, [config], { map, layers }).then(test).then(done, done);
   });
 });
